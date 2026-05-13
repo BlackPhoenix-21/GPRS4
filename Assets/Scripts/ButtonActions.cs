90c731141a4d24c7cedd6a2a98a1737c04bdea37
loilika <kox.arina2003@gmail.com>
2026-05-13 15:13:14 +0200

    button_funktionirt

--- Assets/Scripts/ButtonActions.cs
+++ Assets/Scripts/ButtonActions.cs
@@ -1,81 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ButtonActions : MonoBehaviour
 {
-    private List<GameObject> parentLayer = new List<GameObject>();
+    [SerializeField] private List<GameObject> layers = new List<GameObject>();
 
-    [SerializeField]
-    private GameObject lowLayer;
-
-    [SerializeField]
-    private GameObject highLayer;
-
-    [SerializeField]
-    private List<GameObject> layer = new List<GameObject>();
-
-    [SerializeField]
-    private List<Button> buttons = new List<Button>();
-
-    private void Start()
+    public void OpenLayer(int index)
     {
-        GetParentLayer();
-        DeactivateLayer();
-        ActivateButtons();
-        SetLowLayer();
-        SetHighLayer(parentLayer[0]);
-        buttons[0].interactable = false;
-        layer[0].SetActive(true);
-    }
-
-    private void GetParentLayer()
-    {
-        foreach (GameObject obj in layer)
+        if (index < 0 || index >= layers.Count)
         {
-            parentLayer.Add(obj.transform.parent.gameObject);
+            Debug.LogError("Falscher Layer Index: " + index);
+            return;
         }
-    }
 
-    private void SetLowLayer()
-    {
-        foreach (GameObject obj in parentLayer)
-        {
-            obj.transform.SetParent(lowLayer.transform);
-        }
-    }
+        layers[index].transform.SetAsLastSibling();
 
-    private void SetHighLayer(GameObject obj)
-    {
-        obj.transform.SetParent(highLayer.transform);
-    }
-
-    private void DeactivateLayer()
-    {
-        foreach (GameObject obj in layer)
-        {
-            obj.SetActive(false);
-        }
-    }
-
-    private void ActivateButtons()
-    {
-        foreach (Button button in buttons)
-        {
-            button.interactable = true;
-        }
-    }
-
-    public void ActivateLayer(int layerIndex)
-    {
-        DeactivateLayer();
-        ActivateButtons();
-        if (layerIndex >= 0 && layerIndex < layer.Count)
-        {
-            buttons[layerIndex].interactable = false;
-            layer[layerIndex].SetActive(true);
-            SetLowLayer();
-            SetHighLayer(parentLayer[layerIndex]);
-        }
+        Debug.Log("Open Layer: " + index);
     }
 }
