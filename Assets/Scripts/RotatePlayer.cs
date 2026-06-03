26653c0d84b61f613b0695a7e67019c29dfdeb49
loilika <kox.arina2003@gmail.com>
2026-06-03 15:06:31 +0200

    RotatePlayer

---
+++ Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class RotatePlayer : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Transform character;
+    public float rotationSpeed = 150f;
+
+    private bool isDragging = false;
+
+    void Update()
+    {
+        if (!isDragging) return;
+        if (Mouse.current == null) return;
+
+        float mouseX = Mouse.current.delta.ReadValue().x;
+        character.Rotate(0, -mouseX * rotationSpeed * Time.deltaTime, 0);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+}
